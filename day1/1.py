308a71912f71cd6330b6437221902817277ab250
Jacob de Graaf <jacob.degraaf@justeattakeaway.com>
2021-12-01 20:53:36 +0100

    Day 1

---
+++ day1/1.py
@@ -0,0 +1,12 @@
+f = open("puzzle_input.txt", "r")
+pi = list(map(int, f.read().splitlines()))
+
+part1 = sum([current > previous for previous, current in zip(pi, pi[1:])])
+
+part2 = 0
+for x in range(len(pi) - 3):
+    if sum(pi[x:x + 3]) < sum(pi[x + 1:x + 4]):
+        part2 += 1
+
+print(part1)
+print(part2)
